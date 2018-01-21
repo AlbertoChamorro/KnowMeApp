e808fa04c48c2ce946ccc37bfe5b7bf2ee325e18
Alberto Chamorro <albertchc04@gmail.com>
2018-01-21 15:46:54 -0600

    add design create account and new toolbar

--- app/src/main/java/com/knowme/knowme/View/CreateAccountActivity.java
+++ app/src/main/java/com/knowme/knowme/View/CreateAccountActivity.java
@@ -2,6 +2,7 @@ package com.knowme.knowme.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 import com.knowme.knowme.R;
 
@@ -11,5 +12,16 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        this.showToolbar(getResources().getString(R.string.text_create_account), false);
+    }
+
+    public void showToolbar(String title, Boolean showBackButton) {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(showBackButton);
+    }
+
+    private void setSupportActionBar(Toolbar toolbar) {
     }
 }
