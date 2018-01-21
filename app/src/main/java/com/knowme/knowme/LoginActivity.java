3c79f01e22265d83bfdb8d1197f113cc1633edee
Alberto Chamorro <albertchc04@gmail.com>
2018-01-20 22:02:30 -0600

    finish screen login

--- app/src/main/java/com/knowme/knowme/LoginActivity.java
+++ app/src/main/java/com/knowme/knowme/LoginActivity.java
@@ -2,6 +2,7 @@ package com.knowme.knowme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,4 +11,9 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    public void createNewAccount(View view) {
+
+    }
+
 }
