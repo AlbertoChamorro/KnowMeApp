68930a2fd8886742b6542a7b4fc1dbc73b95a426
Alberto Chamorro <albertchc04@gmail.com>
2018-01-27 12:39:09 -0600

    design button bar, button navigation view and menu items
    bug change color like fav in cardview

---
+++ app/src/main/java/com/knowme/knowme/View/MainActivity.java
@@ -0,0 +1,15 @@
+package com.knowme.knowme.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.knowme.knowme.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
