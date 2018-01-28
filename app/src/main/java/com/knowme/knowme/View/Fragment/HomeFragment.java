bdc1d049a9193b1f2fe54104a4b42f0d125a326a
Alberto Chamorro <albertchc04@gmail.com>
2018-01-28 15:44:12 -0600

    implement recycler view, adapters, view holders.

--- app/src/main/java/com/knowme/knowme/View/Fragment/HomeFragment.java
+++ app/src/main/java/com/knowme/knowme/View/Fragment/HomeFragment.java
@@ -3,11 +3,19 @@ package com.knowme.knowme.View.Fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.knowme.knowme.R;
+import com.knowme.knowme.View.Adapter.PictureAdapterRecyclerView;
+import com.knowme.knowme.View.Model.Picture;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,7 +32,38 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        this.showToolbar("Home",false, view);
+
+        RecyclerView picturesRecylcler = (RecyclerView) view.findViewById(R.id.pictureCardRecyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        picturesRecylcler.setLayoutManager(linearLayoutManager);
+
+        PictureAdapterRecyclerView pictureAdapterRecyclerView = new PictureAdapterRecyclerView(this.buildPictures(), R.layout.cardview_picture, getActivity());
+        picturesRecylcler.setAdapter(pictureAdapterRecyclerView);
+
+        return view;
     }
 
+    public ArrayList<Picture> buildPictures() {
+        ArrayList<Picture> pictures = new ArrayList<>();
+        pictures.add(new Picture(
+                "https://www.google.com.ni/imgres?imgurl=https%3A%2F%2Foinsidercom.files.wordpress.com%2F2017%2F03%2F2nqzjyqi.png&imgrefurl=https%3A%2F%2Foinsidercom.wordpress.com%2F2017%2F01%2F17%2Fin-this-tutorial-we-learn-how-to-implement-qr-code-scanner-generator-using-zxing-scanner-generator-library-in-android-application-2%2F&docid=-ihLMI3SKaQUbM&tbnid=RrfLyer-GLR-NM%3A&vet=10ahUKEwiE4ca2zfvYAhWRvVMKHctFB-AQMwg8KAAwAA..i&w=400&h=400&itg=1&bih=796&biw=1707&q=generator%20images%20picasso%20android&ved=0ahUKEwiE4ca2zfvYAhWRvVMKHctFB-AQMwg8KAAwAA&iact=mrc&uact=8","José Rámirez","10 días","450"
+        ));
+        pictures.add(new Picture(
+                "https://www.google.com.ni/imgres?imgurl=https%3A%2F%2Foinsidercom.files.wordpress.com%2F2017%2F03%2F2nqzjyqi.png&imgrefurl=https%3A%2F%2Foinsidercom.wordpress.com%2F2017%2F01%2F17%2Fin-this-tutorial-we-learn-how-to-implement-qr-code-scanner-generator-using-zxing-scanner-generator-library-in-android-application-2%2F&docid=-ihLMI3SKaQUbM&tbnid=RrfLyer-GLR-NM%3A&vet=10ahUKEwiE4ca2zfvYAhWRvVMKHctFB-AQMwg8KAAwAA..i&w=400&h=400&itg=1&bih=796&biw=1707&q=generator%20images%20picasso%20android&ved=0ahUKEwiE4ca2zfvYAhWRvVMKHctFB-AQMwg8KAAwAA&iact=mrc&uact=8","Alberto Castellón","15 días","4"
+        ));
+        pictures.add(new Picture(
+                "https://www.google.com.ni/imgres?imgurl=https%3A%2F%2Foinsidercom.files.wordpress.com%2F2017%2F03%2F2nqzjyqi.png&imgrefurl=https%3A%2F%2Foinsidercom.wordpress.com%2F2017%2F01%2F17%2Fin-this-tutorial-we-learn-how-to-implement-qr-code-scanner-generator-using-zxing-scanner-generator-library-in-android-application-2%2F&docid=-ihLMI3SKaQUbM&tbnid=RrfLyer-GLR-NM%3A&vet=10ahUKEwiE4ca2zfvYAhWRvVMKHctFB-AQMwg8KAAwAA..i&w=400&h=400&itg=1&bih=796&biw=1707&q=generator%20images%20picasso%20android&ved=0ahUKEwiE4ca2zfvYAhWRvVMKHctFB-AQMwg8KAAwAA&iact=mrc&uact=8","Josh Warren","3 días","50"
+        ));
+       return pictures;
+    }
+
+    public void showToolbar(String title, Boolean showBackButton, View view) {
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle(title);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(showBackButton);
+    }
 }
---
+++ app/src/main/java/com/knowme/knowme/View/Model/Picture.java
@@ -0,0 +1,52 @@
+package com.knowme.knowme.View.Model;
+
+/**
+ * Created by BETO on 27/01/2018.
+ */
+
+public class Picture {
+
+    private String  pictureUrl;
+    private String  userName;
+    private String  time;
+    private String  likeNumber = "0";
+
+    public Picture(String pictureUrl, String userName, String time, String likeNumber) {
+        this.pictureUrl = pictureUrl;
+        this.userName = userName;
+        this.time = time;
+        this.likeNumber = likeNumber;
+    }
+
+    public String getPictureUrl() {
+        return pictureUrl;
+    }
+
+    public void setPictureUrl(String pictureUrl) {
+        this.pictureUrl = pictureUrl;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getLikeNumber() {
+        return likeNumber;
+    }
+
+    public void setLikeNumber(String likeNumber) {
+        this.likeNumber = likeNumber;
+    }
+}
