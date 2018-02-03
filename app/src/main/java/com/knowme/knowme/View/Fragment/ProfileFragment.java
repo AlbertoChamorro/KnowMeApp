e1494557d08a7d70df86cd3fb6d164ddd3290a6c
Alberto <achamorro@coresystems.io>
2018-02-02 19:53:16 -0600

    add simple-view-behavior libraries materials effect dependencies, image-circle-view

--- app/src/main/java/com/knowme/knowme/View/Fragment/ProfileFragment.java
+++ app/src/main/java/com/knowme/knowme/View/Fragment/ProfileFragment.java
@@ -3,11 +3,19 @@ package com.knowme.knowme.View.Fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.knowme.knowme.Adapter.PictureAdapterRecyclerView;
+import com.knowme.knowme.Model.Picture;
 import com.knowme.knowme.R;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,7 +32,40 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        this.showToolbar("", true, view);
+
+        RecyclerView picturesRecylcler = (RecyclerView) view.findViewById(R.id.profileRecyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        picturesRecylcler.setLayoutManager(linearLayoutManager);
+
+        PictureAdapterRecyclerView pictureAdapterRecyclerView = new PictureAdapterRecyclerView(this.buildPictures(), R.layout.cardview_picture, getActivity());
+        picturesRecylcler.setAdapter(pictureAdapterRecyclerView);
+
+        return view;
+    }
+
+
+    public ArrayList<Picture> buildPictures() {
+        ArrayList<Picture> pictures = new ArrayList<>();
+        pictures.add(new Picture(
+                "https://losviajesdedomi.com/wp-content/uploads/2014/02/halong-bay-1-600x402.jpg","José Rámirez","10 días","450 Me gusta"
+        ));
+        pictures.add(new Picture(
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTg-FVz4v6DogPagv-eIq4pTGxEp3w4P_Eq4oxGTIUHyJ6lWizhAA","Alberto Castellón","15 días","4 Me gusta"
+        ));
+        pictures.add(new Picture(
+                "https://i2.wp.com/medioambienteynaturaleza.com/wp-content/uploads/2015/06/Fondos-de-pantalla-de-paisajes-naturales25.jpg","Josh Warren","3 días","50 Me gusta"
+        ));
+        return pictures;
+    }
+
+    public void showToolbar(String title, Boolean showBackButton, View view) {
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle(title);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(showBackButton);
     }
 
 }
