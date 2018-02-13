f95878c15f9f6ea5c0bf57da22febf6bb013e3a9
Alberto Chamorro <albertchc04@gmail.com>
2018-02-13 06:29:52 -0600

    search view custom

--- app/src/main/java/com/knowme/knowme/View/Fragment/SearchFragment.java
+++ app/src/main/java/com/knowme/knowme/View/Fragment/SearchFragment.java
@@ -3,9 +3,11 @@ package com.knowme.knowme.View.Fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.knowme.knowme.R;
 
@@ -24,7 +26,14 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        SearchView searchView = (SearchView) view.findViewById(R.id.search);
+        EditText searchEditText = (EditText)searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text);
+        searchEditText.setTextColor(getResources().getColor(R.color.colorWhite));
+        searchEditText.setHintTextColor(getResources().getColor(R.color.colorWhite));
+        searchView.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+
+        return view;
     }
 
 }
