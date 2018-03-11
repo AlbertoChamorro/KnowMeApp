193d64df2828542dcaf04ec7a349762b321e961f
Alberto <achamorro@coresystems.io>
2018-03-10 19:26:38 -0600

    intent implicito

--- app/src/main/java/com/knowme/knowme/LoginActivity.java
+++ app/src/main/java/com/knowme/knowme/LoginActivity.java
@@ -1,12 +1,15 @@
 package com.knowme.knowme;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import com.knowme.knowme.View.CreateAccountActivity;
 import com.knowme.knowme.View.MainActivity;
+
+import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -24,5 +27,10 @@ public class LoginActivity extends AppCompatActivity {
     public void login(View view) {
        Intent homeActivity = new Intent(this, MainActivity.class);
        startActivity(homeActivity);
+    }
+
+    public void redirectToWebPage(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.marca.com"));
+        startActivity(intent);
     }
 }
