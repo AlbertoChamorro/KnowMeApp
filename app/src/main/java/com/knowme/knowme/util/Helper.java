1c563d13ff66b249759130f6ba64629ba0d8e5d0
Alberto <achamorro@coresystems.io>
2018-03-16 10:33:33 -0600

    helper piccaso load image

--- app/src/main/java/com/knowme/knowme/util/Helper.java
+++ app/src/main/java/com/knowme/knowme/util/Helper.java
@@ -1,7 +1,12 @@
 package com.knowme.knowme.util;
 
+import android.app.Activity;
 import android.text.SpannableString;
 import android.text.style.UnderlineSpan;
+import android.widget.ImageView;
+
+import com.knowme.knowme.R;
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by coredeveloper on 3/15/18.
@@ -13,5 +18,19 @@ public class Helper {
         SpannableString content = new SpannableString(text);
         content.setSpan(new UnderlineSpan(), 0, text.length(), 0);
         return content;
+    }
+
+    public static void LoadImage(Activity context, String url, ImageView container, Integer placeHolder,  Integer errorImage){
+
+        placeHolder = placeHolder == null ? R.drawable.placeholder : placeHolder;
+        errorImage = errorImage == null ? R.drawable.placeholder : errorImage;
+
+        Picasso.with(context)
+                .load(url)
+                .placeholder(placeHolder.intValue())
+                .error(errorImage.intValue())
+                // .resize(50, 50)
+                // .centerCrop()
+                .into(container);
     }
 }
