fb0e899f512c7d83229e79ced464e2594db6946a
Alberto <achamorro@coresystems.io>
2018-03-15 18:14:22 -0600

    create permission hardware camera, integrate

--- app/src/main/java/com/knowme/knowme/post/view/HomeFragment.java
+++ app/src/main/java/com/knowme/knowme/post/view/HomeFragment.java
@@ -1,15 +1,20 @@
-package com.knowme.knowme.view.fragment;
+package com.knowme.knowme.post.view;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.knowme.knowme.R;
 import com.knowme.knowme.adapter.PictureAdapterRecyclerView;
@@ -23,16 +28,26 @@ import java.util.ArrayList;
 public class HomeFragment extends Fragment {
 
 
+    private static final int REQUEST_CAMERA_IDENTIFIER = 1;
+    private FloatingActionButton cameraFloatingButton;
+
     public HomeFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        this.cameraFloatingButton = (FloatingActionButton) view.findViewById(R.id.camera_floating_button);
+        this.cameraFloatingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                takePicture();
+            }
+        });
         this.showToolbar(getResources().getString(R.string.text_home_bottombar),false, view);
 
         RecyclerView picturesRecylcler = (RecyclerView) view.findViewById(R.id.pictureCardRecyclerView);
@@ -44,6 +59,25 @@ public class HomeFragment extends Fragment {
         picturesRecylcler.setAdapter(pictureAdapterRecyclerView);
 
         return view;
+    }
+
+    private void takePicture() {
+        Intent takeCameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (takeCameraIntent.resolveActivity(getActivity().getPackageManager()) != null){
+            startActivityForResult(takeCameraIntent, REQUEST_CAMERA_IDENTIFIER);
+        }
+    }
+
+    // override activity result
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d("Activity Result", "Test !!!");
+        if (requestCode == REQUEST_CAMERA_IDENTIFIER && resultCode == getActivity().RESULT_OK){
+            //Intent intent = new Intent(getActivity(), NewPictureActivity.class);
+            //getActivity().startActivity(intent);
+            Log.d("Home Fragment", "Camera SuccessFull !!!");
+        }
     }
 
     public ArrayList<Picture> buildPictures() {
