452529ec241a57b19dc2444ade520515ba21ddbe
Alberto <achamorro@coresystems.io>
2018-03-20 12:54:26 -0600

    custom toolbar

--- app/src/main/java/com/knowme/knowme/auth/view/CreateAccountActivity.java
+++ app/src/main/java/com/knowme/knowme/auth/view/CreateAccountActivity.java
@@ -8,7 +8,9 @@ import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.knowme.knowme.R;
@@ -20,6 +22,9 @@ public class CreateAccountActivity extends AppCompatActivity implements ICreateA
 
     private static final String TAG = "Create Account Activity";
     private Toolbar toolbar;
+    private TextView titleToolbar;
+    private ImageView imageLeftToolbar;
+    private ImageView imageRightToolbar;
 
     private TextInputEditText emailTextInputEditText, nameTextInputEditText, user_nameTextInputEditText,
             passwordTextInputEditText, confirm_passwordTextInputEditText;
@@ -35,7 +40,8 @@ public class CreateAccountActivity extends AppCompatActivity implements ICreateA
         setContentView(R.layout.activity_create_account);
 
         this.createAccountPresenter = new CreateAccountPresenter(this);
-        this.showToolbar(getResources().getString(R.string.text_create_account), true);
+        //this.showToolbar(getResources().getString(R.string.text_create_account), true);
+        this.setupToolbar();
 
         this.viewContainer = (View) findViewById(R.id.container);
         this.emailTextInputEditText = (TextInputEditText) findViewById(R.id.email);
@@ -122,15 +128,44 @@ public class CreateAccountActivity extends AppCompatActivity implements ICreateA
 
     @Override
     public void goToLogin(String message) {
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
+//        Intent intent = new Intent(this, LoginActivity.class);
+//        startActivity(intent);
+        onBackPressed();
         Helper.showToast(this, message, Toast.LENGTH_LONG);
     }
 
-    public void showToolbar(String title, Boolean showBackButton) {
-        this.toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(this.toolbar);
-        getSupportActionBar().setTitle(title);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(showBackButton);
+//    public void showToolbar(String title, Boolean showBackButton) {
+//        this.toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(this.toolbar);
+//        getSupportActionBar().setTitle(title);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(showBackButton);
+//    }
+
+    private void setupToolbar(){
+
+        //this.toolbar = (Toolbar) findViewById(R.id.toolbar);
+        this.titleToolbar = (TextView) findViewById(R.id.title_toolbar);
+        this.titleToolbar.setText(getResources().getString(R.string.text_create_account));
+
+        this.imageLeftToolbar = (ImageView) findViewById(R.id.left_image_toolbar);
+        //this.imageLeftToolbar.setImageResource(R.drawable.ic_action_logout);
+
+        this.imageLeftToolbar.setBackgroundResource(R.drawable.ic_action_arrow_left);
+
+        this.imageLeftToolbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        this.imageRightToolbar = (ImageView) findViewById(R.id.right_image_toolbar);
+        this.imageRightToolbar.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
     }
 }
