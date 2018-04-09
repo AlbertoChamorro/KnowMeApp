474e9a6aab3a8a350ce5b5abec982ed139eb24cf
Alberto <achamorro@coresystems.io>
2018-04-09 05:54:48 -0600

    implement splash activity

---
+++ app/src/main/java/com/knowme/knowme/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.knowme.knowme;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.knowme.knowme.auth.view.LoginActivity;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 2500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
