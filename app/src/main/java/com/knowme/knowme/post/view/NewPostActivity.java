a8e7f2ac3beb4660b8acfe97987f74e452fbd6e1
Alberto <achamorro@coresystems.io>
2018-04-09 12:59:12 -0600

    fix problem toolbar in create new post

--- app/src/main/java/com/knowme/knowme/post/view/NewPostActivity.java
+++ app/src/main/java/com/knowme/knowme/post/view/NewPostActivity.java
@@ -3,7 +3,9 @@ package com.knowme.knowme.post.view;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.knowme.knowme.R;
 import com.knowme.knowme.util.Helper;
@@ -11,17 +13,16 @@ import com.knowme.knowme.util.Helper;
 public class NewPostActivity extends AppCompatActivity {
 
     private ImageView mImageViewDetailHeader;
-    private Toolbar toolbar;
+    private TextView titleToolbar;
+    private ImageView imageLeftToolbar;
+    private ImageView imageRightToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_post);
 
-        this.toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(this.toolbar);
-        getSupportActionBar().setTitle("Registrar Post");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        this.setupToolbar();
 
         this.mImageViewDetailHeader = (ImageView) findViewById(R.id.image_view_detail_header);
 
@@ -29,5 +30,30 @@ public class NewPostActivity extends AppCompatActivity {
             String photoPath = getIntent().getExtras().getString("PHOTO_PATH_TEMP");
             Helper.LoadImage(this, photoPath, mImageViewDetailHeader, null, null);
         }
+    }
+
+    private void setupToolbar(){
+
+        this.titleToolbar = (TextView) findViewById(R.id.title_toolbar);
+        this.titleToolbar.setText(getResources().getString(R.string.text_create_account));
+
+        this.imageLeftToolbar = (ImageView) findViewById(R.id.left_image_toolbar);
+
+        this.imageLeftToolbar.setBackgroundResource(R.drawable.ic_action_arrow_left);
+
+        this.imageLeftToolbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        this.imageRightToolbar = (ImageView) findViewById(R.id.right_image_toolbar);
+        this.imageRightToolbar.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
     }
 }
