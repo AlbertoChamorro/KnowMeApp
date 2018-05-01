9a3e5f86e61a1434bd534e466ca7ca78fd037d66
Alberto <achamorro@coresystems.io>
2018-04-30 21:37:04 -0600

    delete code innecessary

--- app/src/main/java/com/knowme/knowme/view/fragment/ProfileFragment.java
+++ app/src/main/java/com/knowme/knowme/view/fragment/ProfileFragment.java
@@ -33,39 +33,6 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
-        this.showToolbar("", true, view);
-
-        RecyclerView picturesRecylcler = (RecyclerView) view.findViewById(R.id.profileRecyclerView);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
-        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-        picturesRecylcler.setLayoutManager(linearLayoutManager);
-
-        PictureAdapterRecyclerView pictureAdapterRecyclerView = new PictureAdapterRecyclerView(this.buildPictures(), R.layout.cardview_picture, getActivity());
-        picturesRecylcler.setAdapter(pictureAdapterRecyclerView);
-
         return view;
     }
-
-
-    public ArrayList<Picture> buildPictures() {
-        ArrayList<Picture> pictures = new ArrayList<>();
-        pictures.add(new Picture(
-                "https://losviajesdedomi.com/wp-content/uploads/2014/02/halong-bay-1-600x402.jpg","José Rámirez","10 días","450 Me gusta"
-        ));
-        pictures.add(new Picture(
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTg-FVz4v6DogPagv-eIq4pTGxEp3w4P_Eq4oxGTIUHyJ6lWizhAA","Alberto Castellón","15 días","4 Me gusta"
-        ));
-        pictures.add(new Picture(
-                "https://i2.wp.com/medioambienteynaturaleza.com/wp-content/uploads/2015/06/Fondos-de-pantalla-de-paisajes-naturales25.jpg","Josh Warren","3 días","50 Me gusta"
-        ));
-        return pictures;
-    }
-
-    public void showToolbar(String title, Boolean showBackButton, View view) {
-        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
-        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
-        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle(title);
-        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(showBackButton);
-    }
-
 }
